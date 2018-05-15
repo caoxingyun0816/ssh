1fa8aa84c3c8758a8b7819ee24eaa7c27c9fc84c
caoxingyun <1194441452@qq.com>
2018-05-15 18:04:52 +0800

    ds

---
+++ src/main/java/com/cxy/action/HomePage.java
@@ -0,0 +1,7 @@
+package com.cxy.action;
+
+/**
+ * Created by Administrator on 2018/5/15.
+ */
+public class HomePage {
+}
---
+++ src/main/java/com/cxy/dao/basedao/GenericDao.java
@@ -0,0 +1,7 @@
+package com.cxy.dao.basedao;
+
+/**
+ * Created by Administrator on 2018/5/14.
+ */
+public class GenericDao {
+}
